3378cf5e1babd61e543bc75cafc1e71e57821f2b
Puru <purushottambanerjee@gmail.com>
2024-07-24 09:20:59 +0530

    Project Init

---
+++ backEnd/NotesCollab/src/main/java/com/puru/NotesCollab/NotesCollabApplication.java
@@ -0,0 +1,13 @@
+package com.puru.NotesCollab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesCollabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesCollabApplication.class, args);
+	}
+
+}
